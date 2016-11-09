964f800e2a5afe5b763fbbf014a68ddfc0cb755c
Salgal1975 <salgal@list.ru>
2016-11-09 16:38:35 +0500

    2 commit

--- vrem_god.py
+++ vrem_god.py
@@ -2,3 +2,18 @@
 
 
 def vreminaGoda (month):
+    god = [(12, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11)]
+    if month in god[0]:
+        return print('Зима')
+    elif month in god[1]:
+        return print('Весна')
+    elif month in god[2]:
+        return print('Лето')
+    elif month in god[3]:
+        return print('Осень')
+
+
+
+if __name__ == '__main__':
+
+   vreminaGoda(int(input('Введите номер месяца:')))
