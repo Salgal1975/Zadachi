455eefdd694cabe3d42b732f0c44849a49a45c82
Salgal1975 <salgal@list.ru>
2016-11-09 18:45:03 +0500

    21 cjmm

--- bank.py
+++ bank.py
@@ -2,14 +2,44 @@
 
 
 def bank(vklad, years):
-    for i in 10:
-        vklad += vklad/100*10
-    return print(vklad)
+    for i in range(years):
+        vklad *= 1.1
+        print (vklad)
+    return vklad
 
 
 
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
-    bank((int(input('Введите сумму вклада:'))), (input('На сколько лет:')))
+
+
+
+import unittest
+
+
+class BankTestCase(unittest.TestCase):
+
+    def test_one_year(self):
+
+        self.assertAlmostEqual(bank(10000, 1), 11000)
+
+    def test_normal(self):
+
+        self.assertAlmostEqual(bank(1234, 3), 1642.454)
+        self.assertAlmostEqual(bank(2345, 4), 3433.3145)
+
+    def test_no_money(self):
+
+        for years in range(1, 10):
+            self.assertEqual(bank(0, years), 0)
+
+    def test_zero_years(self):
+
+        self.assertEqual(bank(200, 0), 200, "Случай вклада на 0 лет тоже нужно рассматривать")
+
+
+if __name__ == "__main__":
+    bank (int (input ('Введите сумму вклада:')),int (input ('На сколько лет:')))
+    unittest.main()
