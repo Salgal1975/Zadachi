33a15ff80bae5e888d2943767c9d1738a3e02238
Salgal1975 <salgal@list.ru>
2016-11-09 23:31:32 +0500

    111

--- fibanachi.py
+++ fibanachi.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
+import math
 
 def is_prime(namber):
 
+     if namber == 1:
+         print(False)
+         return False
+
+     for i in range(2, namber):
+         if namber % i == 0:
+             print(False)
+             return False
+     print(True)
+     return True
 
 
-
-if __name__==__main--:
+if __name__ == "__main__":
+    is_prime(int(input("Ввидите число:")))
