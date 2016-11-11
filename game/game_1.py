64f2be3b15e3270e46f678bf81ae2b8fe6a834ea
Salgal1975 <salgal@list.ru>
2016-11-11 20:10:41 +0500

    new

--- game/game_1.py
+++ game/game_1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import tkinter
+import random
 
 # constans
 WIDTH = 640
@@ -11,6 +12,8 @@ MAIN_BALL_RADIUS = 30
 MAIN_BALL_COLOR = 'blue'
 INIT_DX = 1
 INIT_DY = 1
+DELAY = 1
+COLORS = ['aqua', 'fuchsia', 'pink', 'yellow', 'gold']
 
 # balls class
 class Balls():
@@ -40,6 +43,8 @@ class Balls():
                             self.y + self.r,
                             fill=BG_COLOR,      # цвет заполнения круга
                             outline=BG_COLOR)   # цвет линии окружности
+
+
     # метод движения круга
     def move(self):
         # callding with walls
@@ -53,32 +58,53 @@ class Balls():
         self.draw()
 
 
-
-
 # mause events
 def mause_click(event):
     global main_ball # экземпляр класса Balls(), обьявляем глобальной
     if event.num == 1:
-        main_ball = Balls(event.x, event.y, MAIN_BALL_RADIUS, MAIN_BALL_COLOR, INIT_DX, INIT_DY) # экземпляр класса Balls()
-        main_ball.draw()
-        main_ball.move()
-    else:
-        main_ball.hide()
-    #print(event.num, event.x, event.y)
+        if 'main_ball'not in globals():
+            main_ball = Balls(event.x, event.y, MAIN_BALL_RADIUS, MAIN_BALL_COLOR, INIT_DX, INIT_DY) # экземпляр класса Balls()
+            main_ball.draw()
+        else:  # turn left
+            if main_ball.dx * main_ball.dy > 0:
+                main_ball.dy = -main_ball.dy
+            else:
+                main_ball.dx = -main_ball.dx
+    elif event.num == 3:  # turn right
+        if main_ball.dx * main_ball.dy > 0:
+            main_ball.dx = -main_ball.dx
+        else:
+            main_ball.dy = -main_ball.dy
+
+
+# create list of ball
+def create_list_of_balls(number):
+    lst = []
+    while len(lst) < number:
+        next_ball = Balls(random.choice(range(0, WIDTH)),
+                          random.choice(range(0, HEIGHT)),
+                          random.choice(range(15, 35)),
+                          random.choice(COLORS))
+        lst.append(next_ball)
+        next_ball.draw()
+    return lst
+
 
 # main cicle game
 def main():
     if 'main_ball' in globals():
         main_ball.move()
-    root.after(10, main)
+    root.after(DELAY, main)
 
 root = tkinter.Tk()  # экземпляр класса tkinter.Tk()-главне окно
 root.title("Coding balls")  # название окна
 canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)  # свойства рабочего поля
 canvas.pack()  # создает рабочее поле
-canvas.bind('<Button-1>', mause_click)  # назначаем события кнопкам мыши - ПРАВАЯ
-canvas.bind('<Button-2>', mause_click, '+')  # назначаем события кнопкам мыши - ЛЕВАЯ
-canvas.bind('<Button-3>', mause_click, '*')  # назначаем события кнопкам мыши - СРЕДНЯЯ
+canvas.bind('<Button-1>', mause_click)  # назначаем события кнопкам мыши - ЛЕВАЯ
+canvas.bind('<Button-3>', mause_click, '*')  # назначаем события кнопкам мыши - ПРАВАЯ
+if 'main_ball' in globals():
+    del main_ball
+balls = create_list_of_balls(5)
 main()
 root.mainloop()  # запуск главного окна
 
