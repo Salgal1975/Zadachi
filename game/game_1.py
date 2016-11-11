83eff72d2f5643949575a601f14cee5bb4f53260
Salgal1975 <salgal@list.ru>
2016-11-11 16:29:40 +0500

    new 1

--- game/game_1.py
+++ game/game_1.py
@@ -2,10 +2,33 @@
 
 import tkinter
 
+# constans
+WIDTH = 640
+HEIGHT = 480
+BG_COLOR = 'white'
+
+# balls class
+class Balls():
+    def __init__(self, x, y, r, color, dx=0, dy=0):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.color = color
+        self.dx = dx
+        self.dy = dy
+
+# mause events
+def mause_click(event):
+    print(event.num, event.x, event.y)
+
+
 root = tkinter.Tk()
 root.title("Coding balls")
-canvas = tkinter.Canvas(root, width=300, height=300)
+canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)
 canvas.pack()
+canvas.bind('<Button-1>', mause_click)
+canvas.bind('<Button-2>', mause_click, '+' )
+canvas.bind('<Button-3>', mause_click, '*')
 root.mainloop()
 
 
