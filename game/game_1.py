ff5e8a884b6822a5b4448c51b8ed622654c109be
Salgal1975 <salgal@list.ru>
2016-11-11 17:21:19 +0500

    new2

--- game/game_1.py
+++ game/game_1.py
@@ -9,6 +9,7 @@ BG_COLOR = 'white'
 
 # balls class
 class Balls():
+    # конструктор
     def __init__(self, x, y, r, color, dx=0, dy=0):
         self.x = x
         self.y = y
@@ -17,18 +18,43 @@ class Balls():
         self.dx = dx
         self.dy = dy
 
+    # метод рисуем круг
+    def draw(self):
+        canvas.create_oval(self.x - self.r,
+                           self.y - self.r,
+                           self.x + self.r,
+                           self.y + self.r,
+                           fill=self.color)    # цвет заполнения круга
+
+
+    # метод прячем круг
+    def hide(self):
+        canvas.create_oval (self.x - self.r,
+                            self.y - self.r,
+                            self.x + self.r,
+                            self.y + self.r,
+                            fill=BG_COLOR,      # цвет заполнения круга
+                            outline=BG_COLOR)   # цвет линии окружности
+
+
 # mause events
 def mause_click(event):
-    print(event.num, event.x, event.y)
+    global main_ball
+    if event.num == 1:
+        main_ball = Balls(event.x, event.y, 30, 'blue')
+        main_ball.draw()
+    else:
+        main_ball.hide()
+    #print(event.num, event.x, event.y)
 
 
-root = tkinter.Tk()
-root.title("Coding balls")
-canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)
-canvas.pack()
-canvas.bind('<Button-1>', mause_click)
-canvas.bind('<Button-2>', mause_click, '+' )
-canvas.bind('<Button-3>', mause_click, '*')
-root.mainloop()
+root = tkinter.Tk()  # экземпляр класса tkinter.Tk()-главне окно
+root.title("Coding balls")  # название окна
+canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)  # свойства рабочего поля
+canvas.pack()  # создает рабочее поле
+canvas.bind('<Button-1>', mause_click)  # назначаем события кнопкам мыши - ПРАВАЯ
+canvas.bind('<Button-2>', mause_click, '+')  # назначаем события кнопкам мыши - ЛЕВАЯ
+canvas.bind('<Button-3>', mause_click, '*')  # назначаем события кнопкам мыши - СРЕДНЯЯ
+root.mainloop()  # запуск главного окна
 
 
