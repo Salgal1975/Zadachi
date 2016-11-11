52283eaa13b973daf2ec2e42823e78ec6ad4bf0b
Salgal1975 <salgal@list.ru>
2016-11-11 18:54:49 +0500

    new

--- game/game_1.py
+++ game/game_1.py
@@ -6,6 +6,11 @@ import tkinter
 WIDTH = 640
 HEIGHT = 480
 BG_COLOR = 'white'
+ZERO = 0
+MAIN_BALL_RADIUS = 30
+MAIN_BALL_COLOR = 'blue'
+INIT_DX = 1
+INIT_DY = 1
 
 # balls class
 class Balls():
@@ -37,18 +42,26 @@ class Balls():
                             outline=BG_COLOR)   # цвет линии окружности
     # метод движения круга
     def move(self):
-        self.draw()
+        # callding with walls
+        if (self.x + self.r + self.dx >= WIDTH) or (self.x - self.r + self.dx <= ZERO):
+           self.dx = -self.dx
+        if (self.y + self.r + self.dy >= HEIGHT) or (self.y - self.r + self.dy <= ZERO):
+           self.dy = -self.dy
+        self.hide()
         self.x += self.dx
         self.y += self.dy
-        self.hide()
+        self.draw()
+
+
 
 
 # mause events
 def mause_click(event):
     global main_ball # экземпляр класса Balls(), обьявляем глобальной
     if event.num == 1:
-        main_ball = Balls(event.x, event.y, 30, 'blue', 1, 1) # экземпляр класса Balls()
+        main_ball = Balls(event.x, event.y, MAIN_BALL_RADIUS, MAIN_BALL_COLOR, INIT_DX, INIT_DY) # экземпляр класса Balls()
         main_ball.draw()
+        main_ball.move()
     else:
         main_ball.hide()
     #print(event.num, event.x, event.y)
@@ -57,7 +70,7 @@ def mause_click(event):
 def main():
     if 'main_ball' in globals():
         main_ball.move()
-    root.after(10, main())
+    root.after(10, main)
 
 root = tkinter.Tk()  # экземпляр класса tkinter.Tk()-главне окно
 root.title("Coding balls")  # название окна
