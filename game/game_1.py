5702cc9d3ef6ab752a8b0e677b6900f426ace1da
Salgal1975 <salgal@list.ru>
2016-11-11 15:33:33 +0500

    new

--- game/game_1.py
+++ game/game_1.py
@@ -4,7 +4,7 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("Coding balls")
-canvas = tkinter.Canvas(root, width=300, heigh=300)
+canvas = tkinter.Canvas(root, width=300, height=300)
 canvas.pack()
 root.mainloop()
 
