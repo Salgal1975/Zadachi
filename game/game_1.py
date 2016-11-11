d7d35145969451d565b92603d31a253914c5aa0f
Salgal1975 <salgal@list.ru>
2016-11-11 15:30:16 +0500

    new

---
+++ game/game_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import tkinter
+
+root = tkinter.Tk()
+root.title("Coding balls")
+canvas = tkinter.Canvas(root, width=300, heigh=300)
+canvas.pack()
+root.mainloop()
+
+
