afb45edd39bc0b018b32559dd11538b58eb59ae3
Salgal1975 <salgal@list.ru>
2016-11-12 21:31:00 +0500

    tr1

---
+++ vkapi/search_groups_vk.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import vk
+from my_data import myVkData
+
+session = vk.AuthSession(app_id=myVkData.APP_ID, user_login=myVkData.LOGIN, user_password=myVkData.GET_PASSWORD())
+vkapi = vk.API(session)
+
+groups = vkapi.groups.search(q='Python', count=36)
+
+groups_count = groups[0]
+print('Количество групп по теме:%d'%(groups_count))
+
+groups = groups[1:]
+
+[print(group) for group in groups]
