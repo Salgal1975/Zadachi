65250866ca88e516707228a6976b6f4f8a239987
Salgal1975 <salgal@list.ru>
2016-11-12 11:12:02 +0500

    2

--- vkapi/user_get_reqests.py
+++ vkapi/user_get_reqests.py
@@ -3,7 +3,10 @@
 import requests
 from my_data import myVkData
 
-response = requests.get(myVkData.API_URL+'user.get',
-                        {'user_ids': myVkData.DUROV_USER_ID+','+myVkData.MY_USER_ID})
+response = requests.get(myVkData.API_URL+'users.get',
+                        {'user_ids':myVkData.DUROV_USER_ID+','+myVkData.MY_USER_ID+','+myVkData.TOKEN})
 result = response.text
 print(result)
+
+#"?q=Вася&access_token=" + token;"
+#','+myVkData.MY_USER_ID
